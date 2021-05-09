7fcd34f9e2ee8f3f0f10bca40279a609047226e8
Carter Snook <cartersnook04@gmail.com>
2021-05-09 18:29:24 -0500

    feat: implement baseline for element attributes api

--- src/element.rs
+++ src/element.rs
@@ -1,10 +1,38 @@
+use crate::NamedNodeMap;
+use std::sync::{Arc, Mutex, MutexGuard, RwLock, RwLockReadGuard};
+
 #[derive(Debug)]
-pub struct Element<'a> {
-	tag_name: &'a str,
+pub struct Element {
+	tag_name: Arc<&'static str>,
+	attrs: Arc<RwLock<NamedNodeMap>>,
 }
 
-impl<'a> Element<'a> {
-	pub const fn new(tag_name: &'a str) -> Self {
-		Self { tag_name }
+impl Element {
+	pub fn new(tag_name: &'static str) -> Self {
+		Self {
+			tag_name: Arc::new(tag_name),
+			attrs: Arc::new(RwLock::new(NamedNodeMap::new())),
+		}
+	}
+
+	pub fn attributes_lock(&self) -> Arc<RwLock<NamedNodeMap>> {
+		self.attrs.clone()
+	}
+}
+
+#[cfg(test)]
+mod tests {
+	use super::Element;
+	use crate::Attr;
+
+	#[test]
+	fn basic_functionality() {
+		let el = Element::new("h1");
+		let lock = el.attributes_lock();
+		{
+			let mut attributes = lock.try_write().unwrap();
+			attributes.add(Attr::new("data-name", "carter"));
+		}
+		assert_eq!(lock.try_read().unwrap().length(), 1);
 	}
 }

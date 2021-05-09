2658e050fb948508ec8887728884e50b70b18085
Carter Snook <cartersnook04@gmail.com>
2021-05-09 11:42:11 -0500

    feat(attr): add single and multi threaded tests

--- src/attr.rs
+++ src/attr.rs
@@ -18,10 +18,6 @@ impl Attr {
 		*self.name
 	}
 
-	/// Gets the value of attribute at the time of function call.
-	///
-	/// Note: Although this does not stop you from mutating dereferenced value from
-	/// the mutex guard, you should abstain from doing so. Instead, use [`Attr.set_value`].
 	pub fn value(&self) -> &'static str {
 		*self.value_guard()
 	}
@@ -35,5 +31,59 @@ impl Attr {
 	pub fn set_value(&self, new_value: &'static str) {
 		let mut guard = self.value_guard();
 		*guard = new_value;
+	}
+}
+
+#[cfg(test)]
+mod tests {
+	use super::Attr;
+	use std::{
+		sync::{Arc, Mutex},
+		thread,
+	};
+
+	#[test]
+	fn basic_binding() {
+		let attr = Attr::new("type", "text");
+		assert_eq!("type", attr.name());
+		assert_eq!("text", attr.value());
+	}
+
+	#[test]
+	fn single_threaded_mutations() {
+		let attr = Attr::new("type", "text");
+		assert_eq!("text", attr.value());
+		attr.set_value("password");
+		assert_eq!("password", attr.value());
+	}
+
+	#[test]
+	fn single_threaded_guards() {
+		let attr = Attr::new("type", "text");
+		{
+			let mut value_guard = attr.value_guard();
+			assert_eq!(*value_guard, "text");
+			*value_guard = "password";
+			assert_eq!(*value_guard, "password");
+		}
+		assert_eq!(attr.value(), "password");
+	}
+
+	#[test]
+	fn multithreaded_mutations() {
+		let attr = Arc::new(Mutex::new(Attr::new("type", "text")));
+		let attr2 = attr.clone();
+
+		let handler = thread::spawn(move || {
+			let guard = attr2.try_lock().unwrap();
+			assert_eq!("text", guard.value());
+			guard.set_value("foo");
+		});
+
+		handler.join().unwrap();
+
+		let guard = attr.try_lock().unwrap();
+		assert_eq!(guard.name(), "type");
+		assert_eq!(guard.value(), "foo");
 	}
 }

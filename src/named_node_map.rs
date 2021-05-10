e9f0f47496fe16ba0e9aa8a77f014810fbf69b0b
Carter Snook <cartersnook04@gmail.com>
2021-05-09 19:17:21 -0500

    feat: implement tests for named node map

--- src/named_node_map.rs
+++ src/named_node_map.rs
@@ -31,3 +31,49 @@ impl NamedNodeMap {
 		}
 	}
 }
+
+#[cfg(test)]
+mod tests {
+	use super::NamedNodeMap;
+	use crate::Attr;
+	use std::{
+		sync::{Arc, Mutex},
+		thread,
+	};
+
+	const ITER_COUNT: usize = 25;
+
+	#[test]
+	fn singlethreaded_mutations() {
+		let store = Arc::new(Mutex::new(NamedNodeMap::new()));
+
+		for _ in 0..ITER_COUNT {
+			let mut map = store.try_lock().unwrap();
+			map.add(Attr::new("type", "text"));
+		}
+
+		assert_eq!(store.try_lock().unwrap().length(), ITER_COUNT);
+	}
+
+	#[test]
+	fn multithreaded_mutations() {
+		let store = Arc::new(Mutex::new(NamedNodeMap::new()));
+		let mut handlers = vec![];
+
+		for _ in 0..ITER_COUNT {
+			// We need to clone to stop the thread closure from moving the original store.
+			let store = store.clone();
+			let handler = thread::spawn(move || {
+				let mut map = store.lock().unwrap();
+				map.add(Attr::new("type", "text"));
+			});
+			handlers.push(handler);
+		}
+
+		for handle in handlers {
+			handle.join().unwrap();
+		}
+
+		assert_eq!(store.lock().unwrap().length(), ITER_COUNT);
+	}
+}
